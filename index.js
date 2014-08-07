57327aa7eecc0c1aa0359dd6bcd57a27ada7c967
Greg Allen <me@jga.me>
2014-08-07 14:48:35 -0700

    initial work on support for lab 4.0.0

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = function(Lab) {
+  //check if lab 4.0.0
+  var lab = Lab;
+  if (typeof Lab.script == 'function') {
+    lab = exports.lab = Lab.script();
+  }
   global.expect = Lab.expect;
-  global.describe = Lab.describe;
-  global.it = Lab.it;
-  global.before = Lab.before;
-  global.after = Lab.after;
-  global.beforeEach = Lab.beforeEach;
-  global.afterEach = Lab.afterEach;
+  global.describe = lab.describe;
+  global.it = lab.it;
+  global.before = lab.before;
+  global.after = lab.after;
+  global.beforeEach = lab.beforeEach;
+  global.afterEach = lab.afterEach;
 
   global.it.skip = function() {};
   global.describe.skip = function() {};
